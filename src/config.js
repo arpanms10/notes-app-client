9aa87b5c473e09656f22e1f9d09462dee602d1c5
Arpan Mukherjee <arpan@Arpans-MacBook-Pro.local>
2019-05-08 11:32:07 +0800

    updated routes and index

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-2',
+    BUCKET: 'notes-react-app'
+  },
+  apiGateway: {
+    REGION: 'us-east-2',
+    URL: 'https://en32zoqn47.execute-api.us-east-2.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-2',
+    USER_POOL_ID: 'us-east-2_f0eC0EAE7',
+    APP_CLIENT_ID: '2j00n1fpje6h0m24k9t1is1bq4',
+    IDENTITY_POOL_ID: 'us-east-2:7e503f34-d6cc-4a6c-9525-551e908d5e72'
+  }
+};
